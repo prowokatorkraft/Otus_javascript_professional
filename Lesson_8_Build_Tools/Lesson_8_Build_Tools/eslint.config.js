b6138865c0d9692438c0b56bed5790d4b9ed9503
Danil <prowokatorkraft>
2025-12-20 17:27:18 +0400

    Lesson_8_Build_Tools

---
+++ Lesson_8_Build_Tools/Lesson_8_Build_Tools/eslint.config.js
@@ -0,0 +1,24 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-console': 'warn', // "error" or "off"
+      semi: ['error', 'always'], // ;
+    },
+  },
+];
